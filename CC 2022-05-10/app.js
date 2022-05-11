be9546b4f03fe037742997eb8ead41005d5a3c51
Shire224 <shire_hassen224@hotmail.com>
2022-05-10 22:55:52 -0400

    updated answer.

--- CC 2022-05-10/app.js
+++ CC 2022-05-10/app.js
@@ -16,35 +16,35 @@
 //       return [];
 //   }
 
-  function eachCons(array, n) {
-    let newArr = [];
-    let childArr;
-      for(let i = 0; i < array.length; i++) {
-      childArr = array.slice(i, n + i)
-      if(childArr.length === n) {
-        newArr.push(childArr)
-      }
-    }
-    return newArr
-  }
-
 //   function eachCons(array, n) {
-//     const newArr = [];
-//     for (let a=n, i=0; a <= array.length; i++, a++){
-//       newArr.push(array.slice(i,a))
+//     let newArr = []; //New array to push the small arrays in
+//     let childArr; //small arrays where the length will be determined by n
+//       for(let i = 0; i < array.length; i++) { //loop thru the array for each elemnt
+//       childArr = array.slice(i, n + i) //small area will be the modify version of the origan array, starting with the orignal element and stopping at n + element 
+//       if(childArr.length === n) { //if the length of the small array is the same as n then add it to new Array
+//         newArr.push(childArr)
+//       }
 //     }
 //     return newArr
 //   }
 
-// function eachCons(array, n) {
-//     let res = [];
+  function eachCons(array, n) {
+    const newArr = []; //create a new array to push the correct small arrays
+    for (let a=n, i=0; a <= array.length; i++, a++){ //reassign n to a, now loop till the second last element and add one to a and i
+      newArr.push(array.slice(i,a)) //first time loop i = 0 and n = 1, so you slice(start,end) which is (0,2), next loop will be (1,3), next (2,4) --thats if n=2
+    }                               //if n = 3 then i = 0 and, so splice(0,3) for the first loop, next loop slice(1,4), then you push these arrays to the newArray
+    return newArr //son't forget to return the newArray so it doesn't vanish after you call the function
+  }
+
+function eachCons(array, n) {
+    let res = [];//create array to hold the arrays
     
-//     for(let i = 0; i < array.length; i++){
-//       res.push(array.slice(i,i + n));
-//     }
-    
-//     return res.filter(e => e.length === n);
-//   }
+    for(let i = 0; i < array.length; i++){  //simple for loop set up
+      res.push(array.slice(i,i + n));       //end array by stating i + n, which would be 0 + 2 = 2, slice(0,2), next loop will be i + n (1 + 2) which is (1, 3)
+    }                                       //if n = 3, i + n would be slice(0 ,0 + 3), next would be slice(1, 1 + 3)
+                                            //then push the results to the new array called res
+    return res.filter(e => e.length === n); //filer res array so the length would only be as long as n
+  }
 
 // const eachCons = (arr, n) => {
 //     let result = []
