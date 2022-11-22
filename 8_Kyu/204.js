5938e8edcfc23df88c7ebed7ec24312f62b71c44
Shire224 <shire_hassen224@hotmail.com>
2022-11-21 19:27:52 -0500

    solution

---
+++ 8_Kyu/204.js
@@ -0,0 +1,25 @@
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+function correct(string){
+    let newString = string.split('');
+    for (var i = 0; i < newString.length; i++) {
+      if (newString[i] == '5') {
+          newString[i] = 'S';
+      } if (newString[i] == '0') {
+          newString[i] = 'O';
+      }
+        if (newString[i] == '1') {
+          newString[i] = 'I';
+        }
+      }
+    return newString.join('');
+    }
